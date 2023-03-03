b3a22c826f62db1fce1a6359b151aee017a37275
anafrediani <123706247+anafrediani@users.noreply.github.com>
2023-03-02 23:03:00 -0300

    Update tabelaLicao.sql

--- tabelaLicao.sql
+++ tabelaLicao.sql
@@ -1,3 +1,108 @@
+create database exer3;
+use exer3;
+
+create table salario(
+salario numeric not null,
+cargo bigint not null,
+primary key (salario)
+);
+
+create table cargo(
+id_cargo bigint not null auto_increment,
+cargo varchar(60) not null,
+data_ini date not null,
+data_fi date,
+salario numeric not null,
+primary key (id_cargo),
+foreign key (salario) references salario (salario)
+);
+
+create table funcionarios(
+id bigint not null auto_increment,
+nome varchar(50) not null, 
+cpf varchar(50) not null unique,
+data_nasc date not null,
+genero varchar(1),
+cargo bigint not null,
+salario numeric not null,
+primary key (id),
+foreign key(salario) references salario(salario),
+foreign key (cargo) references cargo(id_cargo)
+);
+
+
+create table departament0(
+departament varchar(100) not null,
+funcionario bigint not null,
+gerente varchar (200),
+primary key (departament),
+foreign key (funcionario) references funcionarios(id)
+);
+
+
+
+
+prproximo 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+create database email;
+use email;
+create table anexo(
+numero_anexo bigint not null auto_increment,
+link varchar(50),
+nome varchar(50),
+tamanho numeric(10),
+primary key (numero_anexo)
+);
+
+create table ususrios(
+usuario bigint not null auto_increment,
+nome varchar (50),
+primary key (usuario)
+);
+drop table ususrios;
+create table status (
+num_categoria bigint not null auto_increment,
+categoria varchar (50),
+primary key (num_categoria)
+);
+
+create table email(
+assunto bigint not null,
+corpo_texto text,
+ numero_anexo bigint not null,
+ usuario bigint not null,
+ quando datetime,
+ categoria bigint not null,
+ primary key (assunto),
+ foreign key (numero_anexo) references anexo (numero_anexo),
+ foreign key (usuario) references ususrios (usuario),
+ foreign key (categoria) references status (num_categoria)
+);
+
+insert into anexo(link, nome, tamanho) values ("acesse.one/99Ikb", "salsicha de oculos",  9), ("l1nk.dev/gCQfE","touca", 5),("l1nk.dev/b1R9a","fofo",4);
+select *from anexo;
+
+insert into ususrio(nome) values ("ana"),("elsa"),("olaf"),("rena");
+
+insert into status (categoria) values ("rascunho"),("enviando"),("enviado");
+
+insert into email(assunto, corpo_texto,numero_anexo,usuario,quando,categoria) values ("cachorros","fotos dog",1,1,2023-03-02,1);
+join anexo on email.num_anexo=email.num_anexo
+join ususrio on emai.usuario=ususrio.usuario
+join status on email.categoria=status.num_categoria;
+
+
+
+
+
+proximo 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
 create database diagramana;
 use diagramana;
 
@@ -39,9 +144,10 @@ city varchar(30) not null,
 state varchar(30) not null,
 country varchar(30) not null,
 postalCode numeric(5) not null,
-salesRepEmployeeNumber varchar(50) not null,
+salesRepEmployeeNumber bigint not null,
 creditLimit varchar(20),
-primary key (customerNumber)
+primary key (customerNumber),
+foreign key(salesRepEmployeeNumber) references employees(employeeNumber)
 );
 
 create table payments(
@@ -88,11 +194,61 @@ foreign key (productLine) references productlines (productLine)
 );
 
 create table orderdetails(
-orderNumber bigint not null auto_increment,
+orderNumber bigint not null ,
 productCode bigint not null,
 quantilyOrdered numeric (10) not null,
 priceEach numeric(10) not null,
 orderLineNumber numeric(10) not null, 
 primary key (orderNumber),
+foreign key (orderNumber) references orders(orderNumber),
 foreign key (productCode) references products (productCode)
 );
+drop table orderdetails; 
+drop table customers;
+drop table payments;
+drop table orders;
+
+insert into productlines(textDescription,htmlDescription,image) values 
+("estojo capaidade 4 lapis","l1nk.dev/oPPz2","acesse.one/99Ikb"),
+("caneca","acesse.one/bdYda","l1nk.dev/gCQfE"),
+("brinquedo dog","acesse.one/JoqDF","acesse.one/JoqDF"),
+("capinha","l1nk.dev/mjUj9","l1nk.dev/b1R9a");
+select *from productlines;
+
+insert into products(productName,productLine,productScale,productVendor,productDescripition,quantilyInStock ,buyPrice,msrp) values
+("estojo",1,1,"shein","estojo escolar",1500, 2, 40),
+("caneca",2,1,"amazon", "caneca cafe", 220,15,60),
+("brinquedo",3,1,"petcamp","brinquedo dog",400,7,40),
+("capinha",4,1,"phonecase","capa de celular",600,3,40);
+select *from products;
+
+insert into offices(city,phone,addressLine1,addressLine2,state,country,postalCode,territory) values
+("campinas",32890513,"dog fofo","esquina gato","SP","brasil",7456,"Amrica"),
+("campinas",98206333,"mata pinheiro","comida mac","sp","brasil",9635,"america"),
+("capminas",32885566,"maria bilac","sapo pula","sp","brasil",9963,"america"),
+("campinas",14032005,"nascimento lindo","madre teo","sp","brasil",1403,"america");
+select *from offices;
+
+insert into employees(lastName,firstName,extension,email,officeCode,reportsTo,jobTitle) values 
+("fred","carla","atendente","carla@gmil",1,"eu","rh"),
+("frei","fel","atende","fel@gmail",2,"eu","moço"),
+("tobais","cris","atende","cris@gmail",3,"eu","moço"),
+("lu","rafa","atende","rafa@gmail",4,"eu","moça");
+select *from employees;
+
+insert into customers(customerName,contractLastName,contractFirstName,phone,addressLine1,addressLine2,city,state,country,postalCode,salesRepEmployeeNumber,creditLimit) values
+("felipe","freitas","felipe",983895122,"maria bilac","vinte nove","valinhos","sp","brasil",1111,1,2000),
+("ana","fredinhos","carla",323896365,"pinhais","portal","meia sete","barao","sp","brasil",2222,2,4000);
+select *from customers;
+
+insert into payments (custmersNumber,checkNumber,paymentDate,amount) values 
+(1,403,2023-03-02,4516), (2,355,2023-03-02,45587);
+select *from payments;
+
+insert into orders(orderDate,requiredDate,shippedDate,status,comments,customerNumber) values
+(2023-02-01,20233-02-06,2023-02-7,"enviado","estojo lindo",1),
+(2023-01-02,2023-01-05,2023-01-06,"a enviar","brinquedo macio",2);
+select *from orders
+
+insert into orderdetails(orderNumber,productCode,quantilyOrdered ,priceEach,orderLineNumber) values 
+(555,6979,455,548,8498),(555,68610,67484,8631,5461);
